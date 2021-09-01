eae6d54577999987dd75eb213b36ee1c7eedeea6
Chanze Wu <chanze7995@gmail.com>
2021-09-02 01:06:33 +0800

    create project

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
+  <PageHeader/>
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
 </template>
-
+<script>
+import PageHeader from '@/components/PageHeader.vue'
+export default {
+  name: 'App',
+  components: {
+    PageHeader
+  }
+}
+</script>
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <header>
+    <!-- <div :class="['mobileHeader',{ mobileMenuOpen:isMobileMenuOpen }]">
+      <div class="mobileHeader__logoImg" v-show="!isMobileMenuOpen" >
+        <router-link to="/">
+          <img src="@/assets/img/logo.svg" alt="logo">
+        </router-link>
+      </div>
+      <div :class="['mobileHeader__toggleMenuBtn',{ open:isMobileMenuOpen }]" @click="toggleMobileMenu">
+        <Icon icon-name="menu" icon-class="mobileHeader__icon--menu" v-if="!isMobileMenuOpen" />
+        <Icon icon-name="remove" icon-class="mobileHeader__icon--menu" v-if="isMobileMenuOpen" />
+      </div>
+    </div> -->
+    <ul :class="['headerNavbar',{ mobileMenuOpen:isMobileMenuOpen }]">
+      <li class="headerNavbar__item" @click="toggleMobileMenu">
+        <router-link to="/">首頁</router-link>
+      </li>
+      <li class="headerNavbar__item" @click="toggleMobileMenu">
+        <router-link to="/explore">探索咖啡</router-link>
+      </li>
+      <li class="headerNavbar__item" @click="toggleMobileMenu">
+        <router-link to="/shop">線上購物</router-link>
+      </li>
+      <li class="headerNavbar__logoImg">
+        <router-link to="/">
+          <img src="@/assets/img/logo.svg" alt="logo">
+        </router-link>
+      </li>
+      <li class="headerNavbar__item" @click="toggleMobileMenu">
+        <router-link to="/locations">門市據點</router-link>
+      </li>
+      <li class="headerNavbar__item" @click="toggleMobileMenu">
+        <router-link to="/course">咖啡課程</router-link>
+      </li>
+      <li class="headerNavbar__item" @click="toggleMobileMenu">
+        <router-link to="/account">會員</router-link>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/component/_pageHeader.scss';
+</style>
