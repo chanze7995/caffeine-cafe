7fd0abe876d79f56173762f0c53df7b15f082f46
Chanze Wu <chanze7995@gmail.com>
2021-09-09 09:35:12 +0800

    set sass variables config to use in style scoped

---
+++ vue.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+// function resolve (dir) {
+//   return path.join(__dirname, '.', dir)
+// }
+
+// module.exports = {
+//   chainWebpack: config => {
+//     config.module // 修改svg預設處理,
+//       .rule('svg')
+//       .exclude
+//       .add(resolve('src/assets/img/icons'))
+//       .end()
+
+//     config.module // 新增 svg-sprite-loader 設定
+//       .rule('svg-sprite-loader')
+//       .test(/\.svg$/)
+//       .include
+//       .add(resolve('src/assets/img/icons')) // svg圖示的路徑
+//       .end()
+//       .use('svg-sprite-loader')
+//       .loader('svg-sprite-loader')
+//       .options({
+//         symbolId: 'icon-[name]' // 設定svg中symbol中id的值
+//       })
+//   }
+// }
+
+module.exports = {
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        // 路徑根據具體需求更改
+        path.resolve(__dirname, 'src/assets/scss/config/_variables.scss')
+        // path.resolve(__dirname, 'src/assets/styles/mixin.scss')
+      ]
+    }
+  }
+}
